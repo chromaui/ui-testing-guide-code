dc9bacae842f5250aad544b139dc9d63a48bbd1e
winkerVSbecks <varunvachhar@gmail.com>
2022-04-06 10:51:43 -0400

    starting point for interaction testing tutorial

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,11 +9,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
     '@storybook/addon-a11y',
-    '@storybook/addon-interactions',
   ],
-  features: {
-    interactionsDebugger: true,
-  },
   webpackFinal: async (config) => {
     return {
       ...config,
