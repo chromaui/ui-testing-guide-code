9972128c5128747221a5f321676ad99de3adfcde
r0bins-egg <kazuya.suyama@quipper.com>
2022-06-27 17:16:55 +0900

    interaction section done

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  staticDirs: ['../public'],
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ["../public"],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
   ],
   core: {
     builder: {
-      name: 'webpack5',
+      name: "webpack5",
     },
+  },
+  features: {
+    interactionsDebugger: true,
   },
   webpackFinal: async (config) => {
     return {
@@ -22,8 +26,8 @@ module.exports = {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
-          '@emotion/core': toPath('node_modules/@emotion/react'),
-          'emotion-theming': toPath('node_modules/@emotion/react'),
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
         },
       },
     };
