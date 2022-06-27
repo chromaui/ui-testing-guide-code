50a6241a344f4e71c1f29e8391a4e41fb7393ccd
r0bins-egg <kazuya.suyama@quipper.com>
2022-06-27 21:36:16 +0900

    accessibility session

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
     "@storybook/addon-interactions",
   ],
   core: {
