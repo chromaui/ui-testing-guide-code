da6320a3dbdb380df6919fa45224df7d64c0e6e0
Guille <g.granado.g@gmail.com>
2022-08-07 12:38:59 +0200

    interaction - first test

--- src/InboxScreen.stories.js
+++ src/InboxScreen.stories.js
@@ -3,6 +3,9 @@ import { rest } from 'msw';
 import { InboxScreen } from './InboxScreen';
 import { Default as TaskListDefault } from './components/TaskList.stories';
 
+import { within, userEvent, findByRole } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
 export default {
   component: InboxScreen,
   title: 'InboxScreen',
@@ -33,4 +36,24 @@ Error.parameters = {
       }),
     ],
   },
+};
+
+export const PinTask = Template.bind({});
+PinTask.parameters = Default.parameters;
+PinTask.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const getTask = (name) => canvas.findByRole('listitem', { name });
+
+    // Find the task to pin
+    const itemToPin = await getTask('Export logo');
+
+    // Find the pin button
+    const pinButton = await findByRole(itemToPin, 'button', { name: 'pin' });
+
+    // Click the pin button
+    await userEvent.click(pinButton);
+
+    // Check that the pin button is now a unpin button
+    const unpinButton = within(itemToPin).getByRole('button', { name: 'unpin' });
+    await expect(unpinButton).toBeInTheDocument();
 };
