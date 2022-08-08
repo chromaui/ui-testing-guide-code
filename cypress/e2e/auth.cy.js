931ba7afa20cfba0680899a63624c6bc0c1c5ac7
Guille <g.granado.g@gmail.com>
2022-08-08 23:05:32 +0200

    user flow - set up cypress and run first test

---
+++ cypress/e2e/auth.cy.js
@@ -0,0 +1,18 @@
+describe('The Login Page', () => {
+    it('user can authenticate using the login form', () => {
+        const email = 'alice.carr@test.com';
+        const password = 'k12h1k0$5;lpa@Afn';
+
+        cy.visit('/');
+
+        // Fill out the form
+        cy.get('input[name=email]').type(email);
+        cy.get('input[name=password]').type(`${password}`);
+
+        // Click the sign-in button
+        cy.get('button[type=submit]').click();
+
+        // UI should display the user's task list
+        cy.get('[aria-label="tasks"] li').should('have.length', 6);
+    });
+});
