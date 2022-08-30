5a76a724867ee051352eb54606435fe64d073187
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2022-08-30 12:09:49 -0500

    Small changes for visual testing. Looks good.

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,7 +8,9 @@ import {
   Box,
   VisuallyHidden,
 } from '@chakra-ui/react';
-import { BellIcon } from '@chakra-ui/icons';
+//import { BellIcon } from '@chakra-ui/icons';
+import { StarIcon } from '@chakra-ui/icons';
+
 
 export const Task = ({
   task: { id, title, state },
@@ -47,8 +49,9 @@ export const Task = ({
         flex="1 1 auto"
         color={state === 'TASK_ARCHIVED' ? 'gray.400' : 'gray.700'}
         textDecoration={state === 'TASK_ARCHIVED' ? 'line-through' : 'none'}
-        fontSize="md"
-        fontWeight="bold"
+        // fontSize="md"
+        // fontWeight="bold"
+        fontSize="sm"
         isTruncated
         value={title}
         onChange={(e) => onEditTitle(e.target.value, id)}
@@ -59,7 +62,8 @@ export const Task = ({
       flex="none"
       aria-label={state === 'TASK_PINNED' ? 'unpin' : 'pin'}
       variant={state === 'TASK_PINNED' ? 'unpin' : 'pin'}
-      icon={<BellIcon />}
+      // icon={<BellIcon />}
+      icon={<StarIcon />}
       onClick={() => onTogglePinTask(state, id)}
     />
   </Flex>
