9ba4fe57fd312a2bad5c93c390e0d939f31b4536
Hiroaki Suzuki <rirazou05410@gmail.com>
2023-03-09 22:17:26 +0900

    done Composition

--- src/components/Task.js
+++ src/components/Task.js
@@ -40,7 +40,8 @@ export const Task = ({
         flex="1 1 auto"
         color={state === 'TASK_ARCHIVED' ? 'gray.400' : 'gray.700'}
         textDecoration={state === 'TASK_ARCHIVED' ? 'line-through' : 'none'}
-        fontSize="sm"
+        fontSize="md"
+        fontWeight="bold"
         isTruncated
         value={title}
         onChange={(e) => onEditTitle(e.target.value, id)}
