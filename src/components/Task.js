c6151de833286673837c64eb735317301a7b6446
Ivan Vinski <ivan.vinski98@gmail.com>
2023-05-22 15:58:16 +0200

    Change label font size and button icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 import {
   Checkbox,
   Flex,
   IconButton,
   Input,
   Box,
-  VisuallyHidden,
-} from '@chakra-ui/react';
-import { BellIcon } from '@chakra-ui/icons';
+  VisuallyHidden
+} from '@chakra-ui/react'
+import { StarIcon } from '@chakra-ui/icons'
 
 export const Task = ({
   task: { id, title, state },
@@ -21,13 +21,13 @@ export const Task = ({
     as="li"
     _notLast={{
       borderBottom: '1px',
-      borderColor: 'gray.200',
+      borderColor: 'gray.200'
     }}
     h={12}
     bg="white"
     alignItems="center"
     _hover={{
-      bgGradient: 'linear(to-b,  brand.100,  brand.50)',
+      bgGradient: 'linear(to-b,  brand.100,  brand.50)'
     }}
     aria-label={title}
     tabIndex="0"
@@ -47,8 +47,7 @@ export const Task = ({
         flex="1 1 auto"
         color={state === 'TASK_ARCHIVED' ? 'gray.400' : 'gray.700'}
         textDecoration={state === 'TASK_ARCHIVED' ? 'line-through' : 'none'}
-        fontSize="md"
-        fontWeight="bold"
+        fontSize="sm"
         isTruncated
         value={title}
         onChange={(e) => onEditTitle(e.target.value, id)}
@@ -59,19 +58,19 @@ export const Task = ({
       flex="none"
       aria-label={state === 'TASK_PINNED' ? 'unpin' : 'pin'}
       variant={state === 'TASK_PINNED' ? 'unpin' : 'pin'}
-      icon={<BellIcon />}
+      icon={<StarIcon />}
       onClick={() => onTogglePinTask(state, id)}
     />
   </Flex>
-);
+)
 
 Task.propTypes = {
   task: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired
   }),
   onArchiveTask: PropTypes.func.isRequired,
   onTogglePinTask: PropTypes.func.isRequired,
-  onEditTitle: PropTypes.func.isRequired,
-};
+  onEditTitle: PropTypes.func.isRequired
+}
