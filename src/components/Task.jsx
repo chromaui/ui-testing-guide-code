5c404243b4b7bfef13c6a6f87da0890dbcc22173
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-06-05 19:20:51 +0100

    Added the ability to delete a task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,6 +4,7 @@ export default function Task({
   onArchiveTask,
   onTogglePinTask,
   onEditTitle,
+  onDeleteTask,
 }) {
   return (
     <div
@@ -41,7 +42,13 @@ export default function Task({
           onChange={(e) => onEditTitle(e.target.value, id)}
         />
       </label>
-
+      <button
+        aria-label="delete"
+        className="delete-button"
+        onClick={() => onDeleteTask(id)}
+      >
+        <span className="icon-trash" />
+      </button>
       {state !== "TASK_ARCHIVED" && (
         <button
           className="pin-button"
@@ -73,4 +80,6 @@ Task.propTypes = {
   onTogglePinTask: PropTypes.func.isRequired,
   /** Event to change the task title */
   onEditTitle: PropTypes.func.isRequired,
+  /** Event to delete the task */
+  onDeleteTask: PropTypes.func.isRequired,
 };
