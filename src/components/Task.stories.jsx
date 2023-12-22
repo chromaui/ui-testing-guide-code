6a337af77fb5863ae3d3ffa3d5fdf39a444251e3
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-22 10:58:19 +0000

    Task stories finalized and UI changes

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -19,3 +19,35 @@ export const Default = {
     },
   },
 };
+
+export const Pinned = {
+  args: {
+    task: {
+      id: "2",
+      title: "QA dropdown",
+      state: "TASK_PINNED",
+    },
+  },
+};
+
+export const Archived = {
+  args: {
+    task: {
+      id: "3",
+      title: "Write schema for account menu",
+      state: "TASK_ARCHIVED",
+    },
+  },
+};
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = {
+  args: {
+    task: {
+      id: "4",
+      title: longTitleString,
+      state: "TASK_INBOX",
+    },
+  },
+};
